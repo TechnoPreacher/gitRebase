0374267b5ca2cf0ae501322df222d899eab74482
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-08-29 16:14:28 +0300

    add functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,14 @@
 <?php
+//тоже не всё тут хорошо
+function get_profile($id)
+{
+    $profile = get_user_by('id', $id);
+    return $profile;
+}
 
 
 //в таком виде в функции полно ошибок
 function get_user_meta($id, $key)
 {
     return get_metadata('user', $user_id, $key, $single);
-}
-
-//тоже не всё тут хорошо
-function get_profile($id)
-{
-    $profile = get_user_by('id', $id);
-    return $profile;
 }
