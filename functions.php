1dc9a2849fbdfc02095c20ff67b36128be4d335c
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-08-29 16:36:15 +0300

    add functions.php to feature2

--- functions.php
+++ functions.php
@@ -1,7 +1,15 @@
 <?php
 
+
 //в таком виде в функции полно ошибок
 function get_user_meta($id, $key)
 {
     return get_metadata('user', $user_id, $key, $single);
+}
+
+//тоже не всё тут хорошо
+function get_profile($id)
+{
+    $profile = get_user_by('id', $id);
+    return $profile;
 }
